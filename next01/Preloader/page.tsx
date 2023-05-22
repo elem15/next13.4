3e50b252b83dffe8bf971f883efca799ce38e0a6
Elem <elem15@yandex.ru>
2023-05-22 20:56:30 +0300

    feat: pages & layouts

---
+++ next01/Preloader/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Preloader.css';
+
+const Preloader = () => {
+  return <div placeholder="preloader" className="custom-loader"></div>;
+};
+
+export default Preloader;
---
+++ next01/app/about/loading.tsx
@@ -0,0 +1,7 @@
+import Preloader from '@/Preloader/page';
+
+export default function Loading() {
+  return (
+    <Preloader />
+  )
+}

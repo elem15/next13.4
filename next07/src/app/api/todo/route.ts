7f02d9816441665b4e1e740014a6074e102251a1
Elem <elem15@yandex.ru>
2023-06-12 13:14:06 +0300

    feat: add get todo by id

--- next07/src/app/api/todo/route.ts
+++ next07/src/app/api/todo/route.ts
@@ -5,7 +5,8 @@ const DATE_SOURCE_URL = 'https://jsonplaceholder.typicode.com/todos'
 export async function GET(request: Request) {
   const {searchParams} = new URL(request.url)
   const id = searchParams.get('id')
-  const res = await fetch(`${DATE_SOURCE_URL}?id=${id}`)
-  const data = await res.json()
+  const address = `${DATE_SOURCE_URL}${id ? `?id=${id}` : ''}`
+  const res = await fetch(address)
+  const data: Todo | Todo[] = await res.json()
   return NextResponse.json(data)
 }

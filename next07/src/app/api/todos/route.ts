6b4045c219dbadf014de60ec9b2a60e0455d3c31
Elem <elem15@yandex.ru>
2023-06-12 17:07:19 +0300

    fix: bag with Next POST & DELETE

---
+++ next07/src/app/api/todos/route.ts
@@ -0,0 +1,48 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+const DATE_SOURCE_URL = 'https://jsonplaceholder.typicode.com/todos'
+
+export async function GET(request: Request) {
+  const {searchParams} = new URL(request.url)
+  const id = searchParams.get('id')
+  const address = `${DATE_SOURCE_URL}${id ? `?id=${id}` : ''}`
+  const res = await fetch(address)
+  const data: Todo | Todo[] = await res.json()
+  return NextResponse.json(data)
+}
+export async function DELETE(request: Request) {
+    const {searchParams} = new URL(request.url)
+  const id = searchParams.get('id')
+  // work only in prevues  version 13.2 Next
+  // const {id}: Partial<Todo> = await request.json()
+  // const data = await request.text()
+  // const {id} =  JSON.parse(data)
+  
+  if(!id) return NextResponse.json({ "message": "Id is required" })
+  const address = `${DATE_SOURCE_URL}/${id}}`
+  await fetch(address, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      'API-Key': process.env.DATA_API_KEY as string,
+    }
+  })
+    return NextResponse.json({"message": `Todo № ${id} was deleted`})
+}
+export async function POST(request: Request) {
+
+  const body = await request.text()
+  const {searchParams} = new URL(`http://api?${body}`)
+  const {userId, title} = Object.fromEntries(searchParams.entries())
+  if(!userId || !title) return NextResponse.json({ "message": "UserId and Title are required" })
+  const address = `${DATE_SOURCE_URL}`
+  await fetch(address, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'API-Key': process.env.DATA_API_KEY as string,
+    },
+    body: JSON.stringify({userId, title, completed: true})
+  })
+    return NextResponse.json({"message": `Post with title ${title} was created`})
+}
